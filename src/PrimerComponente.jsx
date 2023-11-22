4da5425f3dfbc98a9c31d6980abd7e0a21c0de1c
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-22 05:21:26 +0100

    Update

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,7 +1,12 @@
+const string = "Esto es un texto";
+const number = 123456;
+const array = ["Práctica React", "Hola", 10000];
+const boolean = true;
+const objeto = { nombre: "Práctica de React", duración: 40 };
+const fecha = new Date();
+
 export const PrimerComponente = () => {
   return (
-    <div>
-      <h1>Primer Componente</h1>
-      </div>
-  )
-}
+    <h1>{ array }</h1>
+  );
+};
