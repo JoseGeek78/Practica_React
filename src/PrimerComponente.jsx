632651ccfa4f38fcfc59cac486062485fae95cb7
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-22 05:23:43 +0100

    Update

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -2,11 +2,12 @@ const string = "Esto es un texto";
 const number = 123456;
 const array = ["Práctica React", "Hola", 10000];
 const boolean = true;
+const funcion = () => 1+1
 const objeto = { nombre: "Práctica de React", duración: 40 };
 const fecha = new Date();
 
 export const PrimerComponente = () => {
   return (
-    <h1>{ array }</h1>
+    <h1>{ funcion() }</h1>
   );
 };
