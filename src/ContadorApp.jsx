32c2d1bf111d79ad4016cf05873c54c4b94d327f
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-23 06:15:26 +0100

    Update

--- src/ContadorApp.jsx
+++ src/ContadorApp.jsx
@@ -1,10 +1,9 @@
-
-
 export const ContadorApp = () => {
   return (
     <>
-    <h1>Contador:</h1>
-    <button>Soy un botón</button>
+      <h1>Contador:</h1>
+      <button onClick={ function (event) {console.log(event)}}>Soy un botón</button>
     </>
-  )
+  );
 };
+ 
